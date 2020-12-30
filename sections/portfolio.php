24024d7e52ceea34ea32435d863ba5450d0ce1ab
Sushant Poudel <sushantpaudel@gmail.com>
2020-12-31 01:26:00 +0545

    Assets and Scripts Uploaded

---
+++ sections/portfolio.php
@@ -0,0 +1,150 @@
+<!-- Portfolio Subpage -->
+<section class="pt-page" id="portfolio">
+    <div class="section-inner custom-page-content">
+        <div class="section-title-block second-style">
+            <h2 class="section-title">Portfolio</h2>
+            <h5 class="section-description">My Works</h5>
+        </div>
+
+        <div class="section-content">
+            <div class="row">
+                <div class="col-xs-12 col-sm-12">
+                    <!-- Portfolio Content -->
+                    <div class="portfolio-content">
+
+                        <ul class="portfolio-filters">
+                            <li class="active">
+                                <a class="filter btn btn-sm btn-link" data-group="category_all">All</a>
+                            </li>
+                            <li>
+                                <a class="filter btn btn-sm btn-link" data-group="category_media">Media</a>
+                            </li>
+                            <li>
+                                <a class="filter btn btn-sm btn-link" data-group="category_mockups">Mockups</a>
+                            </li>
+                            <li>
+                                <a class="filter btn btn-sm btn-link" data-group="category_soundcloud">SoundCloud</a>
+                            </li>
+                            <li>
+                                <a class="filter btn btn-sm btn-link" data-group="category_vimeo-videos">Vimeo Videos</a>
+                            </li>
+                            <li>
+                                <a class="filter btn btn-sm btn-link" data-group="category_youtube-videos">YouTube Videos</a>
+                            </li>
+                        </ul>
+
+                        <!-- Portfolio Grid -->
+                        <div class="portfolio-grid four-columns">
+                            <!-- Portfolio Project 1 -->
+                            <figure class="item lbaudio" data-groups='["category_all", "category_soundcloud"]'>
+                                <div class="portfolio-item-img">
+                                    <img src="assets/images/portfolio/1.jpg" alt="SoundCloud Audio" title="" />
+                                    <a href="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/221650664&#038;color=%23ff5500&#038;auto_play=false&#038;hide_related=false&#038;show_comments=true&#038;show_user=true&#038;show_reposts=false&#038;show_teaser=true&#038;visual=true" class="lightbox mfp-iframe" title="SoundCloud Audio"></a>
+                                </div>
+
+                                <i class="fa fa-volume-up"></i>
+                                <h4 class="name">SoundCloud Audio</h4>
+                                <span class="category">SoundCloud</span>
+                            </figure>
+                            <!-- /Portfolio Project 1 -->
+
+                            <!-- Portfolio Project 2 -->
+                            <figure class="item standard" data-groups='["category_all", "category_media"]'>
+                                <div class="portfolio-item-img">
+                                    <img src="assets/images/portfolio/2.jpg" alt="Media Project 2" title="" />
+                                    <a href="portfolio-1.html" class="ajax-page-load"></a>
+                                </div>
+
+                                <i class="far fa-file-alt"></i>
+                                <h4 class="name">Media Project 2</h4>
+                                <span class="category">Media</span>
+                            </figure>
+                            <!-- /Portfolio Project 2 -->
+
+                            <!-- Portfolio Project 3 -->
+                            <figure class="item lbvideo" data-groups='["category_all", "category_vimeo-videos"]'>
+                                <div class="portfolio-item-img">
+                                    <img src="assets/images/portfolio/3.jpg" alt="Vimeo Video 1" title="" />
+                                    <a href="https://player.vimeo.com/video/158284739" class="lightbox mfp-iframe" title="Vimeo Video 1"></a>
+                                </div>
+
+                                <i class="fas fa-video"></i>
+                                <h4 class="name">Vimeo Video 1</h4>
+                                <span class="category">Vimeo Videos</span>
+                            </figure>
+                            <!-- Portfolio Project 3 -->
+
+                            <!-- Portfolio Project 4 -->
+                            <figure class="item standard" data-groups='["category_all", "category_media"]'>
+                                <div class="portfolio-item-img">
+                                    <img src="assets/images/portfolio/4.jpg" alt="Media Project 1" title="" />
+                                    <a href="#" class="ajax-page-load"></a>
+                                </div>
+
+                                <i class="far fa-file-alt"></i>
+                                <h4 class="name">Media Project 1</h4>
+                                <span class="category">Media</span>
+                            </figure>
+                            <!-- Portfolio Project 4 -->
+
+                            <!-- Portfolio Project 5 -->
+                            <figure class="item lbimage" data-groups='["category_all", "category_mockups"]'>
+                                <div class="portfolio-item-img">
+                                    <img src="assets/images/portfolio/5.jpg" alt="Mockup Design 1" title="" />
+                                    <a class="lightbox" title="Mockup Design 1" href="assets/images/portfolio/5.jpg"></a>
+                                </div>
+
+                                <i class="far fa-image"></i>
+                                <h4 class="name">Mockup Design 1</h4>
+                                <span class="category">Mockups</span>
+                            </figure>
+                            <!-- /Portfolio Project 5 -->
+
+                            <!-- Portfolio Project 6 -->
+                            <figure class="item lbvideo" data-groups='["category_all", "category_youtube-videos"]'>
+                                <div class="portfolio-item-img">
+                                    <img src="assets/images/portfolio/6.jpg" alt="YouTube Video 1" title="" />
+                                    <a href="https://www.youtube.com/embed/bg0gv2YpIok" class="lightbox mfp-iframe" title="YouTube Video 1"></a>
+                                </div>
+
+                                <i class="fas fa-video"></i>
+                                <h4 class="name">YouTube Video 1</h4>
+                                <span class="category">YouTube Videos</span>
+                            </figure>
+                            <!-- /Portfolio Project 6 -->
+
+                            <!-- Portfolio Project 7 -->
+                            <figure class="item standard" data-groups='["category_all", "category_media"]'>
+                                <div class="portfolio-item-img">
+                                    <img src="assets/images/portfolio/5.jpg" alt="Media Project 1" title="" />
+                                    <a href="" class="ajax-page-load"></a>
+                                </div>
+
+                                <i class="far fa-file-alt"></i>
+                                <h4 class="name">Media Project 3</h4>
+                                <span class="category">Media</span>
+                            </figure>
+                            <!-- /Portfolio Project 7 -->
+
+                            <!-- Portfolio Project 8 -->
+                            <figure class="item lbimage" data-groups='["category_all", "category_mockups"]'>
+                                <div class="portfolio-item-img">
+                                    <img src="assets/images/portfolio/6.jpg" alt="Mockup Design 1" title="" />
+                                    <a class="lightbox" title="Mockup Design 1" href="assets/images/portfolio/6.jpg"></a>
+                                </div>
+
+                                <i class="far fa-image"></i>
+                                <h4 class="name">Mockup Design 2</h4>
+                                <span class="category">Mockups</span>
+                            </figure>
+                            <!-- /Portfolio Project 8 -->
+
+                        </div>
+                    </div>
+                    <!-- /Portfolio Content -->
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- /Portfolio Subpage -->
